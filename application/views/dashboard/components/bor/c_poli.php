1b77cb6823ef3af3aea5e0463ab94a17017ce2b4
Gilang Pratama <pratamapriadi96@gmail.com>
2021-09-22 00:32:17 +0700

    :zap: add bor

---
+++ application/views/dashboard/components/bor/c_poli.php
@@ -0,0 +1,24 @@
+<?php foreach ($poli as $lists) : ?>
+	<div class="col-lg-3 col-6">
+		<div class="small-box p-2" style="background:#<?= $lists->poli_color ?>">
+			<div class="inner">
+				<h3><?= rand(1000, 9999) ?></h3>
+				<p><?= $lists->poli_deskripsi ?></p>
+			</div>
+			<div class="icon">
+				<i class="fa <?= $lists->poli_icon ?>"></i>
+			</div>
+		</div>
+	</div>
+<?php endforeach ?>
+<div class="col-lg-3 col-6">
+	<div class="small-box p-2" style="background:#B97A95">
+		<div class="inner">
+			<h3><?= rand(1000, 9999) ?></h3>
+			<p>Total</p>
+		</div>
+		<div class="icon">
+			<i class="fa fa-book-medical"></i>
+		</div>
+	</div>
+</div>
