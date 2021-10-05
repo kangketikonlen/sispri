2d3e6694c63ff5c2a9d79ffa31cefec40d89d918
Gilang Pratama <pratamapriadi96@gmail.com>
2021-10-06 05:42:46 +0700

    :zap: update pasien covid

--- application/controllers/Indikator/Pasien_covid.php
+++ application/controllers/Indikator/Pasien_covid.php
@@ -1,75 +1,66 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
-class Pasien_covid extends MY_Controller
+class Pasien_covid extends MY_Dashboard
 {
 	public function __construct()
 	{
 		parent::__construct();
-		$isLogin = $this->session->userdata('LoggedIn');
-		if (!$isLogin) {
-			redirect('portal');
-		} else {
-			$this->load->model('Dashboard/Pasien_covid_model', 'm');
-		}
+		$this->load->model('Indikator/Pasien_covid_model', 'm');
 	}
 
-	public function index()
+	public function get_jumlah()
 	{
-		$data['Root'] = "Dashboard";
-		$data['Title'] = "Pasien Covid";
-		$data['Breadcrumb'] = array();
-		$data['Template'] = "templates/private";
-		$data['Components'] = array(
-			'main' => "/v_private_topbar",
-			'header' => $data['Template'] . "/components/v_header",
-			'navbar' => $data['Template'] . "/components/v_navbar_landing",
-			'footer' => $data['Template'] . "/components/v_footer",
-			'content' => str_replace("/", "/v_", $this->session->userdata('UrlDash'))
-		);
-		$data['poli'] = $this->m->get_data();
-		$this->load->view('v_main', $data);
-	}
-
-	public function chart_data()
-	{
-		$dataset = array();
-		$poli = $this->m->get_data();
-
-		foreach ($poli as $lists) {
-			$dataset[] = array(
-				'label' => $lists->poli_deskripsi,
-				'backgroundColor' => '#' . $lists->poli_color,
-				'borderColor' => '#' . $lists->poli_color,
-				'pointColor' => '#' . $lists->poli_color,
-				'pointStrokeColor' => '#' . $lists->poli_color,
-				'pointHighlightFill' => '#fff',
-				'pointRadius' => false,
-				'data' => array(rand(0, 1000), rand(0, 1000), rand(0, 1000), rand(0, 1000), rand(0, 1000), rand(0, 1000), rand(0, 1000), rand(0, 1000), rand(0, 1000), rand(0, 1000), rand(0, 1000), rand(0, 1000)),
-			);
+		$results = array();
+		$ruangan = $this->m->get_ruangan();
+		foreach ($ruangan as $ruangan) {
+			if ($ruangan->kelas == "ICU") {
+				$results[$ruangan->id] = $this->m->hitung_pasien($ruangan->nama_ruang, $ruangan->kelas);
+			} else {
+				$results[$ruangan->id] = 0;
+			}
 		}
-
-		echo json_encode($dataset);
+		echo json_encode($results);
 	}
 
 	public function get_table()
 	{
+		$tgl_awal = date("Y-m-d H:i:s", strtotime($this->input->get('tanggal_awal') . '- 1 day'));
+		$tgl_akhir = date("Y-m-d H:i:s", strtotime($this->input->get('tanggal_akhir') . '+ 1 day'));
+		// 
 		$data = array();
-
-		for ($i = 1; $i <= 10; $i++) {
-			$data[] = array(
-				'nomor' => $i,
-				'tanggal' => date("Y-m-d"),
-				'pasien' => "Pasien " . $i,
-				'dokter' => "Dokter " . $i,
-				'poli' => $this->m->get_poli($i)
-			);
+		$ruangan = $this->m->get_ruangan();
+		foreach ($ruangan as $ruangan) {
+			if ($ruangan->kelas == "ICU") {
+				$data[$ruangan->kelas] = $this->m->get_table_data($ruangan->nama_ruang, $ruangan->kelas, $tgl_awal, $tgl_akhir);
+			} else {
+				$data[$ruangan->kelas] = 0;
+			}
 		}
-
 		echo json_encode($data);
 	}
 
-	public function logout()
+	public function chart_data()
 	{
-		$this->session->sess_destroy();
-		redirect('portal');
+		$labels = array();
+		$backgroundColor = array();
+		$data = array();
+		$ruangan = $this->m->get_ruangan();
+		$i = 0;
+		foreach ($ruangan as $ruangan) {
+			$labels[] = $ruangan->kelas;
+			$backgroundColor[] = ($i++ % 2 == 0) ? '#39A388' : '#6ECB63';
+			if ($ruangan->kelas == "ICU") {
+				$data[] = $this->m->hitung_pasien($ruangan->nama_ruang, $ruangan->kelas);
+			} else {
+				$data[] = 0;
+			}
+		}
+
+		$data = array(
+			'labels' => $labels,
+			'backgroundColor' => $backgroundColor,
+			'data' => $data
+		);
+
+		echo json_encode($data);
 	}
 }
--- application/models/Indikator/Pasien_covid_model.php
+++ application/models/Indikator/Pasien_covid_model.php
@@ -1,17 +1,40 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 class Pasien_covid_model extends CI_Model
 {
-	protected $poli = "ak_data_master_poli";
+	protected $ruangan = "daftar_ruangan";
+	protected $ri = "data_pendaftaran_ri";
 
-	public function get_data()
+	public function get_ruangan()
 	{
-		$this->db->where($this->poli . '.deleted', false);
-		return $this->db->get($this->poli)->result();
+		$sidawangi = $this->load->database('sdw', TRUE);
+		$sidawangi->where($this->ruangan . '.nama_ruang', $this->input->get('ruangan'));
+		$sidawangi->order_by('kelas');
+		return $sidawangi->get($this->ruangan)->result();
 	}
 
-	public function get_poli($id)
+	public function hitung_pasien($ruang, $kelas)
 	{
-		$this->db->where($this->poli . '.poli_id', $id);
-		return $this->db->get($this->poli)->row('poli_deskripsi');
+		$tgl_awal = date("Y-m-d H:i:s", strtotime($this->input->get('tanggal_awal') . '- 1 day'));
+		$tgl_akhir = date("Y-m-d H:i:s", strtotime($this->input->get('tanggal_akhir') . '+ 1 day'));
+		// 
+		$sidawangi = $this->load->database('sdw', TRUE);
+		$sidawangi->where($this->ri . '.ruang', $ruang);
+		$sidawangi->where($this->ri . '.kelas', $kelas);
+		$sidawangi->where($this->ri . '.date_in>=', $tgl_awal);
+		$sidawangi->where($this->ri . '.date_in<=', $tgl_akhir);
+		$sidawangi->order_by('kelas');
+		return $sidawangi->get($this->ri)->num_rows();
+	}
+
+	public function get_table_data($ruang, $kelas, $tgl_awal, $tgl_akhir)
+	{
+		$sidawangi = $this->load->database('sdw', TRUE);
+		$sidawangi->select('date_in, dokter_rs, count(id) as pasien, kelas');
+		$sidawangi->where($this->ri . '.date_in>=', $tgl_awal);
+		$sidawangi->where($this->ri . '.date_in<=', $tgl_akhir);
+		$sidawangi->where($this->ri . '.ruang', $ruang);
+		$sidawangi->where($this->ri . '.kelas', $kelas);
+		$sidawangi->group_by('dokter_rs');
+		return $sidawangi->get($this->ri)->result_array();
 	}
 }
